da76ef221c5c80e5a34228fd36e3c366685e0479
supreme-01 <kumarsujith565@gmail.com>
2022-01-29 12:14:49 +0530

    first commit

---
+++ dbs-project/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RatingComponent } from './rating/rating.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RatingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
