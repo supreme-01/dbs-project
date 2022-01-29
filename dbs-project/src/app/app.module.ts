6062f1d69cded75aac617debecc41e63b3fca59b
shyampate123 <shyampate123>
2022-01-29 13:43:11 +0530

    second commit with addition of result

--- dbs-project/src/app/app.module.ts
+++ dbs-project/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RatingComponent } from './rating/rating.component';
+import { ResultComponent } from './result/result.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const routes: Routes = [
+  {path:'', component:RatingComponent},
+  {path:'result',component:ResultComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RatingComponent
+    RatingComponent,
+    ResultComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dbs-project/src/app/rating/rating.component.ts
+++ dbs-project/src/app/rating/rating.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rating',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating.component.css']
 })
 export class RatingComponent implements OnInit {
+ public dbsForm:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private http:HttpClient,
+    private router: Router,) { }
 
-  ngOnInit(): void {
+  
+  ngOnInit(){
+    this.initializeForm();
+  }
+  initializeForm() {
+    this.dbsForm=this.formBuilder.group({
+      h1 : new FormControl(''),
+      m1: new FormControl(''),
+      l1: new FormControl(''),
+      h2: new FormControl(''),
+      m2: new FormControl(''),
+      l2: new FormControl(''),
+      h21: new FormControl(''),
+      h3: new FormControl(''),
+      m3: new FormControl(''),
+      l3: new FormControl(''),
+      h31: new FormControl(''),
+      h4: new FormControl(''),
+      m4: new FormControl(''),
+      l4: new FormControl(''),
+      h41: new FormControl(''),
+    })
   }
 
+  onSubmit(){
+    const obj={
+      country:"india",
+      formData:this.dbsForm.value
+    };
+    console.log(obj);
+    this.http.post("http://localhost:8080/",JSON.stringify(obj),{headers: {'Content-Type': 'application/json'}}).subscribe(data=>{
+      console.log(obj);
+      this.router.navigate(['/result']);
+    });
+  }
+  
 }
